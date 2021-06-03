d061b256405693f052dea4e3fb0474faf35365b7
Megan McCarty <megan_mccarty@hotmail.com>
2021-06-03 16:30:11 -0400

    Add fetch and render functions for books

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {});
+
+const listPanel = document.getElementById('list-panel');
+const list = document.getElementById('list');
+const showPanel = document.getElementById('show-panel');
+
+const booksURL = 'http://localhost:3000/books';
+
+function getBooks() {
+    return fetch(booksURL)
+        .then(response => response.json())
+        .then(data => renderBooks(data));
+}
+
+function renderBooks(data) {
+    data.map(bookObj => console.log(bookObj));
+}
