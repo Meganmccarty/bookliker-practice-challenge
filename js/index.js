00a2abeeb2d6cf9e15ea6d6cc03c5f092e5d8cdb
Megan McCarty <megan_mccarty@hotmail.com>
2021-06-03 17:54:05 -0400

    Modify render books, add function for show book details

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", getBooks);
 
 const listPanel = document.getElementById('list-panel');
 const list = document.getElementById('list');
@@ -13,5 +13,37 @@ function getBooks() {
 }
 
 function renderBooks(data) {
-    data.map(bookObj => console.log(bookObj));
+    data.map(bookObj => {
+        const li = document.createElement('li');
+        li.innerText = bookObj.title;
+        list.append(li);
+        li.addEventListener('click', () => showBookDetails(bookObj));
+        return list;
+    });
+}
+
+function showBookDetails(bookObj) {
+    showPanel.innerHTML = '';
+
+    const title = document.createElement('h1');
+    const subtitle = document.createElement('h2');
+    const img = document.createElement('img');
+    const author = document.createElement('h3');
+    const desc = document.createElement('p');
+    const users = document.createElement('ul');
+
+    bookObj.users.map(user => {
+        const userLi = document.createElement('li');
+        userLi.innerText = user.username;
+        users.append(userLi);
+        return users;
+    });
+
+    title.innerText = bookObj.title;
+    subtitle.innerText = bookObj.subtitle;
+    img.src = bookObj.img_url;
+    author.innerText = bookObj.author;
+    desc.innerText = bookObj.description;
+
+    showPanel.append(img, title, subtitle, author, desc, users);
 }
